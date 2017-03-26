8196d407e021532727deee6b133e03a272a56b88
Domagoj Penic <dpenic23@gmail.com>
2017-03-27 01:55:16 +0200

    Implemented functions for calculating cycle lenghts and the longest cycle in a graph.

--- Longest Cycle/main.cpp
+++ Longest Cycle/main.cpp
@@ -7,11 +7,14 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <set>
 
 using namespace std;
 
 int num_of_nodes;
 vector< vector<int> > adjacency_matrix;
+
+vector<int> cycle_lengths;
 
 /*
  * Function takes absolute path to the file where the adjacency matrix
@@ -48,14 +51,55 @@ int read_matrix(char* path){
 	return 1;
 }
 
+/*
+ * Recursive function which finds all the cycle lengths for which
+ * v_0 = v_n = node_index.
+ */
+ void cycle_rec(int start_node, int current_node, set<int>& visited, int length){
+
+	if(start_node == current_node && length > 0){
+		cycle_lengths.push_back(length);
+		return;
+	}
+
+	visited.insert(current_node);
+	vector<int> neighbours = adjacency_matrix[current_node];
+
+	for(int i = 0; i < neighbours.size(); i++){
+		bool is_visited = visited.find(neighbours[i]) != visited.end();
+		if(!is_visited || neighbours[i] == start_node){
+			cycle_rec(start_node, neighbours[i], visited, length + 1);
+		}
+	}
+}
+
+/*
+ * Function which calculates the longest cycle in a simple graph.
+ */
 int longest_cycle(){
-	return 0;
+
+	// Find the lengths of all the cycles
+	for(int i = 0; i < num_of_nodes; i++){
+		set<int> visited;
+		cycle_rec(i, i, visited, 0);
+	}
+	
+	// Find the longest cycle
+	int longest = 0;
+	for(int i = 0; i < cycle_lengths.size(); i++){
+		if(cycle_lengths[i] > longest){
+			longest = cycle_lengths[i];
+		}
+	}
+
+	return longest;
 }
 
 /*
  * Main function, called once the program is run. The program takes
  * only one argument, absolute path to the file where the adjacency
- * matrix is stored.
+ * matrix is stored and prints the length of the longest cycle in the
+ * specified graph.
  */
 int main(int argc, char* argv[]){
 
